1b096a366ce1ab10ddbce9f2639766cc0b0aacbf
David Cantrell <david.l.cantrell@gmail.com>
2008-03-20 00:22:33 +0000

    Updated GPL boilerplates and updated my email address.

--- src/basename.py
+++ src/basename.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 #
 # basename.py - basename(1) command written in Python
+# Copyright (C) 2007  David Cantrell <david.l.cantrell@gmail.com>
 #
-# This copyrighted material is made available to anyone wishing to use,
-# modify, copy, or redistribute it subject to the terms and conditions of
-# the GNU General Public License v.2.  This program is distributed in the
-# hope that it will be useful, but WITHOUT ANY WARRANTY expressed or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-# details.  You should have received a copy of the GNU General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
-# USA. Any Red Hat trademarks that are incorporated in the source code or
-# documentation are not subject to the GNU General Public License and may
-# only be used or replicated with the express permission of Red Hat, Inc.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
-# Author: David Cantrell <dcantrell@redhat.com>
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
 pybasenamever = (1, 0)
